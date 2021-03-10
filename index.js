3f30eebbd2d53479c6107b47224475b53d5b936f
Lambtsa <42414095+Lambtsa@users.noreply.github.com>
2021-03-10 20:23:14 +0100

    linting

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ const { app } = require('./app');
 
 const port = process.env.PORT || 3000;
 
+/* eslint-disable-next-line */
 app.listen(port, () => console.log(`Server listening on http://localhost:${port}`));
