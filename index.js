0db635562d397bd21918ee14d11077944decd398
Lambtsa <42414095+Lambtsa@users.noreply.github.com>
2021-03-10 22:01:16 +0100

    env update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { app } = require('./app');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 /* eslint-disable-next-line */
 app.listen(port, () => console.log(`Server listening on http://localhost:${port}`));
