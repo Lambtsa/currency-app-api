4d664c98f30a789e56be6eb2b8dcdbfc7b67f296
Lambtsa <42414095+Lambtsa@users.noreply.github.com>
2021-03-10 17:13:36 +0100

    initial setup

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.get('/', (req, res) => {
   res
     .status(200)
-    .json({ message: "You are here" })
+    .json({ message: 'You are here' });
 });
 
-module.exports = { app }
+module.exports = { app };
