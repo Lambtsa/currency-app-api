3621427283f9d393f59d3be38aef157e51bc8046
Lambtsa <42414095+Lambtsa@users.noreply.github.com>
2021-03-10 20:49:02 +0100

    initial setup

--- app.js
+++ app.js
@@ -8,4 +8,19 @@ app.get('/', (req, res) => {
     .json({ message: 'You are at the root' });
 });
 
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.statusCode = 404;
+  next(error);
+});
+
+// eslint-disable-next-line
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const name = err.name || 'Error';
+  res
+    .status(statusCode)
+    .json({ name, message: err.message });
+});
+
 module.exports = { app };
