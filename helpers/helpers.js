d527a9a7841b3da2e8fdfae7726fe1d2cf4dd352
Lambtsa <42414095+Lambtsa@users.noreply.github.com>
2021-03-11 15:32:00 +0100

    update to currencies

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,109 +1,146 @@
-const filterCrypto = (data, crypto) => data
-  .filter(r => r.asset_id_quote === crypto)[0]
-  .rate;
+const filterCrypto = (data, crypto) => {
+  const rate = data
+    .filter(r => r.asset_id_quote === crypto)[0]
+    .rate;
+  if (rate < 1) {
+    const rounded = Math.round(rate * 1000000)
+    return (rounded  / 1000000)
+      .toFixed(6);
+  }
+  return rate.toFixed(4)
+}
 
 module.exports.createCryptoRatesObj = data => ({
   baseCurrency: 'EUR',
   rates: [
     {
-      name: 'BCH',
+      initials: 'BCH',
+      name: 'Bitcoin Cash',
       rate: filterCrypto(data.rates, 'BCH'),
       logo: '/assets/flags/bch.webp',
     },
     {
-      name: 'BTC',
+      initials: 'BTC',
+      name: 'Bitcoin',
       rate: filterCrypto(data.rates, 'BTC'),
       logo: '/assets/flags/btc.webp',
     },
     {
+      initials: 'EOS',
       name: 'EOS',
       rate: filterCrypto(data.rates, 'EOS'),
       logo: '/assets/flags/eos.webp',
     },
     {
-      name: 'ETH',
+      initials: 'ETH',
+      name: 'Ethereum',
       rate: filterCrypto(data.rates, 'ETH'),
       logo: '/assets/flags/eth.webp',
     },
     {
-      name: 'HBAR',
+      initials: 'HBAR',
+      name: 'Hedera Hashgraph',
       rate: filterCrypto(data.rates, 'HBAR'),
       logo: '/assets/flags/hbar.webp',
     },
     {
-      name: 'LTC',
+      initials: 'LTC',
+      name: 'Litecoin',
       rate: filterCrypto(data.rates, 'LTC'),
       logo: '/assets/flags/ltc.webp',
     },
   ],
 });
 
+const filterCurrencies = (data, currency) => {
+  const rate = data[currency];
+  if (rate < 1) {
+    const rounded = Math.round(rate * 1000000)
+    return (rounded  / 1000000)
+      .toFixed(6);
+  }
+  return rate.toFixed(4)
+}
+
 module.exports.createCurrencyRatesObj = data => ({
   baseCurrency: 'EUR',
   rates: [
     {
-      name: 'AUD',
-      rate: data.rates.AUD,
+      initials: 'AUD',
+      name: 'Australian Dollar',
+      rate: filterCurrencies(data.rates, 'AUD'),
       logo: '/assets/flags/aud.webp',
     },
     {
-      name: 'CAD',
-      rate: data.rates.CAD,
+      initials: 'CAD',
+      name: 'Canadian Dollar',
+      rate: filterCurrencies(data.rates, 'CAD'),
       logo: '/assets/flags/cad.webp',
     },
     {
-      name: 'CHF',
-      rate: data.rates.CHF,
+      initials: 'CHF',
+      name: 'Swiss Franc',
+      rate: filterCurrencies(data.rates, 'CHF'),
       logo: '/assets/flags/chf.webp',
     },
     {
-      name: 'CNY',
-      rate: data.rates.CNY,
+      initials: 'CNY',
+      name: 'Chinese Yuan',
+      rate: filterCurrencies(data.rates, 'CNY'),
       logo: '/assets/flags/cny.webp',
     },
     {
-      name: 'DKK',
-      rate: data.rates.DKK,
+      initials: 'DKK',
+      name: 'Danish Krone',
+      rate: filterCurrencies(data.rates, 'DKK'),
       logo: '/assets/flags/dkk.webp',
     },
     {
-      name: 'EUR',
-      rate: data.rates.EUR,
+      initials: 'EUR',
+      name: 'Euro',
+      rate: filterCurrencies(data.rates, 'EUR'),
       logo: '/assets/flags/eur.webp',
     },
     {
-      name: 'GBP',
-      rate: data.rates.GBP,
+      initials: 'GBP',
+      name: 'British Pound Sterling',
+      rate: filterCurrencies(data.rates, 'GBP'),
       logo: '/assets/flags/gbp.webp',
     },
     {
-      name: 'HKD',
-      rate: data.rates.HKD,
+      initials: 'HKD',
+      name: 'Hong Kong Dollar',
+      rate: filterCurrencies(data.rates, 'HKD'),
       logo: '/assets/flags/hkd.webp',
     },
     {
-      name: 'JPY',
-      rate: data.rates.JPY,
+      initials: 'JPY',
+      name: 'Japanese Yen',
+      rate: filterCurrencies(data.rates, 'JPY'),
       logo: '/assets/flags/jpy.webp',
     },
     {
-      name: 'MXN',
-      rate: data.rates.MXN,
+      initials: 'MXN',
+      name: 'Mexican Peso',
+      rate: filterCurrencies(data.rates, 'MXN'),
       logo: '/assets/flags/mxn.webp',
     },
     {
-      name: 'NOK',
-      rate: data.rates.NOK,
+      initials: 'NOK',
+      name: 'Norwegian Krone',
+      rate: filterCurrencies(data.rates, 'NOK'),
       logo: '/assets/flags/nok.webp',
     },
     {
-      name: 'SEK',
-      rate: data.rates.SEK,
+      initials: 'SEK',
+      name: 'Swedish Krona',
+      rate: filterCurrencies(data.rates, 'SEK'),
       logo: '/assets/flags/sek.webp',
     },
     {
-      name: 'USD',
-      rate: data.rates.USD,
+      initials: 'USD',
+      name: 'United States Dollar',
+      rate: filterCurrencies(data.rates, 'USD'),
       logo: '/assets/flags/usd.webp',
     },
   ],
