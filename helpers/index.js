94cf0106ae83eb24e5723391e1b43365f51ac0d1
Lambtsa <42414095+Lambtsa@users.noreply.github.com>
2021-03-11 18:42:55 +0100

    update to currency obj

--- helpers/index.js
+++ helpers/index.js
@@ -4,18 +4,18 @@ const { fetchCrypto, fetchCurrencies } = process.env.NODE_ENV === 'production'
 // const { fetchCrypto, fetchCurrencies } = require('./fetch/fetchData');
 const { createCryptoRatesObj, createCurrencyRatesObj } = require('./helpers');
 
-const currencies = async() => {
-  const rawCryptoData =  await fetchCrypto();
-  const rawCurrenciesData =  await fetchCurrencies();
+const currencies = async () => {
+  const rawCryptoData = await fetchCrypto();
+  const rawCurrenciesData = await fetchCurrencies();
   const cryptoData = createCryptoRatesObj(rawCryptoData);
   const currencyData = createCurrencyRatesObj(rawCurrenciesData);
   return {
-    baseCurrency: 'EUR', 
+    baseCurrency: 'EUR',
     rates: [
       ...cryptoData.rates,
       ...currencyData.rates,
-    ] 
-  }
+    ],
+  };
 };
 
 module.exports = {
