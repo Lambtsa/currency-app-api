76c47cae0e2a95e9908a4b047fcbf0b5c7faebd8
Lambtsa <42414095+Lambtsa@users.noreply.github.com>
2021-03-12 08:52:52 +0100

    repairs to endpoint

--- routes/index.js
+++ routes/index.js
@@ -13,10 +13,9 @@ router.get('/', (req, res) => {
 
 router.get('/currencies', async (req, res, next) => {
   try {
-    const currencyData = await currencies;
     res
       .status(200)
-      .json(currencyData);
+      .json(currencies);
   } catch (err) {
     // Need to handle this better
     next(err);
