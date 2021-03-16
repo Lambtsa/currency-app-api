06cd96287e82d9200c5b77b09e7aaba7277d0448
Lambtsa <42414095+Lambtsa@users.noreply.github.com>
2021-03-16 08:37:06 +0100

    update max connections

---
+++ db/db.js
@@ -0,0 +1,17 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  max: 2,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+});
+
+/* eslint-disable */
+module.exports = {
+  query: async (text, params) => await pool.query(text, params),
+};
