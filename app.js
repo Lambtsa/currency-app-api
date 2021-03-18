b7db76c02e4403afb7f5b7cdd02b06c1d0febe46
Lambtsa <42414095+Lambtsa@users.noreply.github.com>
2021-03-18 20:36:35 +0100

    linting

--- app.js
+++ app.js
@@ -44,7 +44,8 @@ cron.schedule('0 */1 * * *', async () => {
     const updatedCurrencies = await getNewRates();
     updatedCurrencies.forEach(async currency => {
       const oldCurrency = await CurrencyDB.find({ initials: currency.initials });
-      const delta = Math.round(((currency.rate - oldCurrency[0].rate) / oldCurrency[0].rate) * 10000) / 10000;
+      const delta = Math
+        .round(((currency.rate - oldCurrency[0].rate) / oldCurrency[0].rate) * 10000) / 10000;
       console.log(delta);
       await CurrencyDB.findOneAndUpdate(
         { initials: currency.initials },
